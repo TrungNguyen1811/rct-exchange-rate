556b490f75309cd36d79457f922a0816d57c931d
TrungNguyen1811 <trung4atlam@gmail.com>
2025-08-18 11:57:07 +0700

    feat: build  structure project and setup

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul className="nav__list">
+        <li className="nav__item">convert</li>
+        <li className="nav__item">history</li>
+        <li className="nav__item">compare</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
